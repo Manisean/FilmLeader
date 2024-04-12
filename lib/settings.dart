c6c766d315201026e72e574f8375490c50e0bc15
IsaacSJones <isaac.jones@mymail.champlain.edu>
2024-04-12 14:15:17 -0400

    minor settings bug fix

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,4 +1,3 @@
-// import 'package:filmhelper/pages.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -28,18 +27,11 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  /*int _selectedFocusGroup1 = -1;
-  int _selectedFocusGroup2 = -1;
-
-  String _selectedFocusGroup1Text = '';
-  String _selectedFocusGroup2Text = '';*/
-  late bool _isDarkMode;
+  bool _isDarkMode = false;
 
   @override
   void initState() {
     super.initState();
-    //_selectedFocusGroup1 = selectedFocusGroup1;
-    //_selectedFocusGroup2 = selectedFocusGroup2;
     _loadDarkModeState();
   }
 
@@ -80,7 +72,7 @@ class _SettingsPageState extends State<SettingsPage> {
                     ),
                   ),
                   Switch(
-                    value: _isDarkMode ?? false,
+                    value: _isDarkMode,
                     onChanged: (value) {
                       setState(() {
                         _isDarkMode = value; // Update local state
@@ -90,85 +82,6 @@ class _SettingsPageState extends State<SettingsPage> {
                       });
                     },
                   ),
-                  /*
-                  DropdownButton<int>(
-                    value: _selectedFocusGroup1,
-                    hint: Text(_selectedFocusGroup1Text.isNotEmpty ? _selectedFocusGroup1Text : 'Select Focus'),
-                    onChanged: (int? value) {
-                      if (value != null) {
-                        setState(() {
-                          _selectedFocusGroup1 = value;
-                          _selectedFocusGroup1Text = ['Little in Focus', 'Some in Focus', 'Lots in Focus'][value];
-                        });
-                      }
-                    },
-                    items: [
-                      DropdownMenuItem<int>(
-                        value: -1,
-                        child: Text('Select Option'),
-                      ),
-                      DropdownMenuItem<int>(
-                        value: 0,
-                        child: Text('Little in Focus'),
-                      ),
-                      DropdownMenuItem<int>(
-                        value: 1,
-                        child: Text('Some in Focus'),
-                      ),
-                      DropdownMenuItem<int>(
-                        value: 2,
-                        child: Text('Lots in Focus'),
-                      ),
-                    ],
-                  ),
-                  const Center(
-                    child: Text(
-                      'MOVEMENT BLUR',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  DropdownButton<int>(
-                    value: _selectedFocusGroup2,
-                    hint: Text(_selectedFocusGroup2Text.isNotEmpty ? _selectedFocusGroup2Text : 'Select Blur'),
-                    onChanged: (int? value) {
-                      if (value != null) {
-                      setState(() {
-                        _selectedFocusGroup2 = value;
-                        _selectedFocusGroup2Text = ['Little Blur', 'Some Blur', 'Lots of Blur'][value];
-                      });
-                      }
-                    },
-                    items: [
-                      DropdownMenuItem<int>(
-                        value: -1,
-                        child: Text('Select Option'),
-                      ),
-                      DropdownMenuItem<int>(
-                        value: 0,
-                        child: Text('Little Blur'),
-                      ),
-                      DropdownMenuItem<int>(
-                        value: 1,
-                        child: Text('Some Blur'),
-                      ),
-                      DropdownMenuItem<int>(
-                        value: 2,
-                        child: Text('Lots of Blur'),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      selectedFocusGroup1 = _selectedFocusGroup1;
-                      selectedFocusGroup2 = _selectedFocusGroup2;
-                    },
-                    child: Text('Save Preferences'),
-                  ),*/
                 ],
               ),
             ),
